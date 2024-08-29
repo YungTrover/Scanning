b1a48f24accbeeb73e14950ac18e9fbdebf1891c
YungTrover <tjhlax@gmail.com>
2024-08-29 16:48:57 -0400

    fixing gobuster_runner I think this one is working

--- gobuster_runner.py
+++ gobuster_runner.py
@@ -2,15 +2,16 @@ import subprocess
  
 
 def run_gobuster(target, port):
-    print(f"Running Gobuster on {target}")
+    print(f"Running Gobuster on http://{target}:{port}/ ")
     default_wordlist = "/usr/share/wordlists/dirb/common.txt"
-    use_custom_wordlist = input("Would you like to use a custom wordlist? (y/N): ").strip().lower()
+    use_custom_wordlist = input("Would you like to use a custom wordlist? (y/n): ").strip().lower()
 
     if use_custom_wordlist == 'y':
-        custom_wordlist = input("Please enter the path to the wordlist you would like to use").strip
+        custom_wordlist = input("Please enter the path to the wordlist you would like to use: ").strip()
         wordlist = custom_wordlist if custom_wordlist else default_wordlist
     else:
         wordlist = default_wordlist
-
-    command = ["gobuster", "dir", "-u ", f"http://{target}:{port}/", " -w" , wordlist ]
-    subprocess.run(command)
+        
+    command = ["gobuster", "dir", "-u", f"http://{target}:{port}/", "-w" , wordlist ]
+    subprocess.run(command, check=True)
+    
